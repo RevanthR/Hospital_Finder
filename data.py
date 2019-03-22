a7afface8a3dea838a8f828a090c879fcc4a93e6
RevanthR <revanthrobbie37@gmail.com>
2019-03-22 21:52:21 +0530

    Backend Completed

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ my_csv = pd.read_csv("hospital_directory.csv")
 column=my_csv['Location_Coordinates']
 column=column.str.split(',')
 Hosp_Name=my_csv['Hospital_Name']
-
+Hosp_Addr=my_csv['Address_Original_First_Line']
 #while i<=5:
 	#print(column[i][0])
 	#i+=1
@@ -16,7 +16,7 @@ for i in range(len(column)):
 lon=[]
 for j in range(len(column)):
 	lon.append(column[j][1])
+def add(l1,l2):
+	a=l1+','+l2
+	return my_csv.loc[my_csv['Location_Coordinates'] == a]
 
-print(type(column[1][1]))
-print(column[1][1])
-print(float(column[1][1]))
--- finder.py
+++ finder.py
@@ -26,11 +26,18 @@ sug_lat=[]
 sug_lon=[]
 for i in range(len(lat_list)):
 	dist=distance(cur_lat,cur_lon,radians(float(lat_list[i])),radians(float(lon_list[i])))
-	if dist <= 15:
+	if dist <= 2:
 		sug_lat.append(lat_list[i])
 		sug_lon.append(lon_list[i])
 
 
-print(sug_lat)
-print(sug_lon)
-
+for i in range(len(sug_lon)):
+	a=data.add(sug_lat[i],sug_lon[i])
+Name=a['Hospital_Name'].to_string(index=False)	
+Address=a['Address_Original_First_Line'].to_string(index=False)
+Name=Name.split('\n')
+Address=Address.split('\n')
+for i in range(len(Name)):
+	print(Name[i].lstrip())
+	print(Address[i].lstrip())
+	print('\n')
