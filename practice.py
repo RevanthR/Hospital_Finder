c6f4a5801ad4f3d05b6b153084323a99c956e54f
RevanthR <revanthrobbie37@gmail.com>
2019-03-25 23:10:36 +0530

    UI incomplete

---
+++ practice.py
@@ -0,0 +1,20 @@
+
+q=[2,1,3,4,5]
+
+s=list(q)
+s.sort()
+print(q)
+print(s)
+print(q[1])
+count=0
+i=0
+#while i<len(q):
+#    if s[i]==q[i]:
+ #       i+=1
+  #  else:
+#            print("Too chaotic")
+            
+  #      else:
+   #    
+     #   i=i+1
+#print(count)
